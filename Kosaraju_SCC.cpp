9c029628d17bd1d69c921d4a3f3bbd87b8ab8568
Kapil Deshpande <kapil.deshpande36@gmail.com>
2021-08-02 12:47:49 +0530

    Create Kosaraju_SCC.cpp

---
+++ Kosaraju_SCC.cpp
@@ -0,0 +1,64 @@
+#include <bits/stdc++.h>
+using namespace std;
+void dfs(int node, stack<int> &st, vector<int> &vis, vector<int> adj[]) {
+    vis[node] = 1; 
+    for(auto it: adj[node]) {
+        if(!vis[it]) {
+            dfs(it, st, vis, adj); 
+        }
+    }
+    
+    st.push(node); 
+}
+void revDfs(int node, vector<int> &vis, vector<int> transpose[]) {
+    cout << node << " "; 
+    vis[node] = 1; 
+    for(auto it: transpose[node]) {
+        if(!vis[it]) {
+            revDfs(it, vis, transpose); 
+        }
+    }
+}
+int main() {
+  int n, m;
+  cin >> n >> m; 
+	vector<int> adj[n]; 
+	for(int i = 0;i<m;i++) {
+	    int u, v;
+	    cin >> u >> v; 
+	    adj[u].push_back(v);
+	}
+	
+  //Topological Sort
+	stack<int> st;
+	vector<int> vis(n, 0); 
+	for(int i = 0;i<n;i++) {
+	    if(!vis[i]) {
+	        dfs(i, st, vis, adj); 
+	    }
+	} 
+	
+  //Transpose Graph -> Reverse every Edge
+	vector<int> transpose[n]; 
+	
+	for(int i = 0;i<n;i++) {
+	    vis[i] = 0; 
+	    for(auto it: adj[i]) {
+	        transpose[it].push_back(i); 
+	    }
+	}
+	
+	//Perform Reverse DFS
+	while(!st.empty()) {
+	    int node = st.top();
+	    st.pop(); 
+	    if(!vis[node]) {
+	        cout << "SCC: "; 
+	        revDfs(node, vis, transpose); 
+	        cout << endl; 
+	    }
+	}
+	
+	
+	return 0;
+}
